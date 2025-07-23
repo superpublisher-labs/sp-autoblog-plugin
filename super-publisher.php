989ed635a85a2b3bb888ac110f95588f8bc97948
Eduardo Santos <4b4c4t3666@gmail.com>
2025-07-23 17:13:42 -0300

    changed the version to make a tutorial of instalation

--- super-publisher.php
+++ super-publisher.php
@@ -3,7 +3,7 @@
 /**
  * Plugin Name: Super Publisher
  * Description: Conecte seu site WordPress ao Super Publisher para automatizar a criação e publicação de conteúdos.
- * Version: 1.2.4
+ * Version: 1.2.4.1
  * Author: Super Publisher
  * Author URI: https://sp-autoblog.test/
  * License: GPL2
